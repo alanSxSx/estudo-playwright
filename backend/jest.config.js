d3f778a019cd4cf1a2fcf7d4f554caa6cccc7666
Alan Barbosa <alanzinhob@hotmail.com>
2025-07-12 12:42:16 -0300

    update

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.js', '**/tests/**/*.spec.js'],
+	collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text', 'text-summary'],
 };
